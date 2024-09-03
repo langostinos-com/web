14d66dcf0abe6c8fd7262c5e4e8d803e8799bfac
leoleguizamon <anlleguizamongu@unal.edu.co>
2024-09-03 14:14:35 -0500

    minfix to deploy

--- src/components/Proximamente.vue
+++ src/components/Proximamente.vue
@@ -1,6 +1,6 @@
 <template>
-	<div class="w-100 h-100 p-2 my-5 text-center">
-		<h1 style="font-family: adelia; font-size: 8vw;">Proximamente</h1>
+	<div class="w-100 h-100 p-5 text-center">
+		<h1 class="my-5" style="font-family: adelia; font-size: 8vw;">Proximamente</h1>
 		<div class="spinner-border" style="height: 200px; width: 200px; animation-duration: 3s;" role="status"></div>
 	</div>
 </template>
